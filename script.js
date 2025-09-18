c9c2c2325f44531e0d605e7d326a97a771237d4d
saboorcode <saboor2malik@gmail.com>
2025-09-17 23:08:45 -0700

    themeSkipByUser() function

--- script.js
+++ script.js
@@ -4,30 +4,51 @@
 const portfolio = document.querySelector(".portfolio");
 const opening = document.querySelector(".star-wars-opening");
 
-//opening.style.display = "none";
-portfolio.style.display = "none";
+/*
+opening.style.display = "none";
+portfolio.style.display = "block";
+
+document.body.style.background = `linear-gradient(rgba(0, 0, 0, 0.70), rgba(0, 0, 0, 0.70)), url("assets/bg/xwing.png") no-repeat center center / cover fixed`;
+typer();
+*/
+
+let userSkipTheme = false;
 
 // Delay: 1000 = 1 second
-// Terminate opening and switch to main portfolio.  Call Typer() function - See "Job Title Typer" code block below.
-setTimeout(()=> {
+// Terminate star wars theme opening and switch to main portfolio.  Invoke typer() function - See "Typer" code block below.
+function themeSwitch() { // execute once
     portfolio.style.display = "block";
     opening.style.display = "none";
     document.body.style.background = `linear-gradient(rgba(0, 0, 0, 0.70), rgba(0, 0, 0, 0.70)), url("assets/bg/xwing.png") no-repeat center center / cover fixed`;
     typer();
-}, 9500);
+}
 
+function themeSkipByUser() {
+    document.addEventListener("click", () => {
+        userSkipTheme = true;
+        themeSwitch();
+    });
+
+    setTimeout(() => {
+        if (!userSkipTheme) {
+            themeSwitch();
+        }
+    }, 9500);
+}
+
+themeSkipByUser();
 
 /*******************/
-/* Job Title Typer */
+/* Typer */
 /*******************/
-// Realistic User Typing with blinking "_"
+// Realistic Typing Animation with blinking underscore "_"
 const jobTitlePara = document.querySelector(".job-title");
 
-const jobTitle = "Aspiring Web Developer";
+const jobTitle = "Aspiring Frontend Developer";
 
-const lastChar = document.createElement("span");
-lastChar.textContent = "_";
-lastChar.style.color = "#EEDE46";
+const underscore = document.createElement("span");
+underscore.textContent = "_";
+underscore.style.color = "#5bee6cff";
 
 jobTitlePara.textContent = "_";
 
@@ -35,20 +56,20 @@ let intervalCount = 0; // Declared and initialized to keep track of character in
 
 // Typer implementation - Realistic User Typing with blinking "_"
 // setInterval() just like a loop, executes code block per delay aka iternation.
-function typer(){
-setInterval(() => {
-    if (jobTitle.length !== intervalCount) { // Type each character until the last character
-        // Utilized replace method to remove "_" and append right after typing the next character
-        // It's instant it looks like "_" was not removed. Inserting substring at an index method isn't a thing.
-        jobTitlePara.textContent = (jobTitlePara.textContent + jobTitle[intervalCount]).replace("_", "");
-        jobTitlePara.append(lastChar);
-        intervalCount++;
-    } else { // Implement blinking "_" - Increases delay for realistic blinking "_"
-        if (jobTitlePara.textContent.includes("_")){
-            jobTitlePara.textContent = jobTitlePara.textContent.replace("_", "");
-        } else {
-            jobTitlePara.append(lastChar);
+function typer() {
+    setInterval(() => {
+        if (jobTitle.length !== intervalCount) { // Type each character until the last character
+            // Utilized replace method to remove "_" and append right after typing the next character
+            // It's instant it looks like "_" was not removed. Inserting substring at an index method isn't a thing.
+            jobTitlePara.textContent = (jobTitlePara.textContent + jobTitle[intervalCount]).replace("_", "");
+            jobTitlePara.append(underscore);
+            intervalCount++;
+        } else { // Implement blinking "_" - Increases delay for realistic blinking "_"
+            if (jobTitlePara.textContent.includes("_")) {
+                jobTitlePara.textContent = jobTitlePara.textContent.replace("_", "");
+            } else {
+                jobTitlePara.append(underscore);
+            }
         }
-    }
-}, 200);
+    }, 200);
 }
